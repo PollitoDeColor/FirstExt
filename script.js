344cde14048828252c720667d955b418faae58d1
PollitoDeColor <140357795+PollitoDeColor@users.noreply.github.com>
2023-08-27 21:12:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ const clientId = 'v3gnon73g7cupdj172jjcgi7p9orao';
 const redirectUri = 'https://pollitodecolor.github.io/FirstExt/overlay.html'; // Debe coincidir con la URL configurada en el Centro de Desarrollo de Twitch
 
 const authButton = document.getElementById('authButton');
-console.log('authButton:', authButton);
 const userInfo = document.getElementById('userInfo');
 const usernameSpan = document.getElementById('username');
 
 authButton.addEventListener('click', async () => {
-    console.log('Auth button clicked');
-  if (!localStorage.getItem('access_token')) {
-      console.log('Authorized:', auth);
-    // Iniciar el flujo de autenticación de Twitch
-    window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=user:read:email`;
-  } else {
-    // Aquí puedes realizar acciones que requieren autenticación
-    // Por ejemplo, obtener información del usuario desde la API de Twitch
-    const accessToken = localStorage.getItem('access_token');
-    fetchUserInfo(accessToken); // Llamar a la función para obtener información del usuario
-  }
+  twitch.ext.actions.requestAuthToken();
+});
+
+twitch.ext.onAuthorized(async (auth) => {
+  const accessToken = auth.token;
+  
+  // Almacena el token de acceso usando twitch.ext.configuration
+  await twitch.ext.configuration.set('access_token', accessToken);
+
+  // Llama a la función para obtener información del usuario
+  fetchUserInfo(accessToken);
 });
 
 async function fetchUserInfo(accessToken) {
@@ -25,18 +24,17 @@ async function fetchUserInfo(accessToken) {
     const response = await fetch('https://api.twitch.tv/helix/users', {
       headers: {
         'Client-ID': clientId,
-        'Authorization': `Bearer ${accessToken}`
+        'Authorization': `Extension ${accessToken}` // Cambia "Bearer" a "Extension"
       }
     });
 
     if (response.ok) {
       const data = await response.json();
-      const username = data.data[0].login; // Obtener el nombre de usuario
-      const capitalizedUsername = username.charAt(0).toUpperCase() + username.slice(1); // Capitaliza la primera letra
+      const username = data.data[0].login;
+      const capitalizedUsername = username.charAt(0).toUpperCase() + username.slice(1);
       usernameSpan.textContent = capitalizedUsername;
       userInfo.style.display = 'block';
 
-      // Actualizar el atributo alt de la imagen del pollito con el nombre de usuario
       const pollitoImage = document.getElementById('pollito');
       pollitoImage.alt = username;
     } else {
@@ -47,22 +45,13 @@ async function fetchUserInfo(accessToken) {
   }
 }
 
-
-// Verificar si ya tenemos un token de acceso almacenado
-const urlFragment = window.location.hash.substring(1);
-const urlParams = new URLSearchParams(urlFragment);
-const accessToken = urlParams.get('access_token');
-
-if (accessToken) {
-  // Limpia el fragmento de URL para que no se muestre en la barra de direcciones
-  window.history.replaceState({}, document.title, window.location.pathname);
-
-  // Almacena el token de acceso en el almacenamiento local
-  localStorage.setItem('access_token', accessToken);
-
-  // Mostrar la información del usuario
-  fetchUserInfo(accessToken); // Llamar a la función para obtener información del usuario
-}
+// Aquí puedes usar twitch.ext.configuration para obtener el token de acceso almacenado
+twitch.ext.configuration.onChanged(async () => {
+  const accessToken = await twitch.ext.configuration.get('access_token');
+  if (accessToken) {
+    fetchUserInfo(accessToken);
+  }
+});
 
   // Ahora continuamos con el resto del código...*/
 
